b97e227fc35a8d536c929de5904c47094cf21dc9
myatminnthu2812 <57025277+myatminnthu2812@users.noreply.github.com>
2020-03-07 23:13:25 +0630

    Add files via upload

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Post;
+use Faker\Generator as Faker;
+
+$factory->define(Post::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence(),
+        'content' => $faker->paragraphs(4,true),
+        'author_id' => \App\User::get()->random()->id,
+        'category_id' => \App\Category::get()->random()->id,
+        'is_published' => $faker->boolean(70)
+    ];
+});
